553226199025121988f7e861468173a46c773288
trnciii <doityan223@gmail.com>
2022-12-20 03:59:17 +0900

    create a command

---
+++ imgdiff/__main__.py
@@ -0,0 +1,39 @@
+import argparse, numpy as np
+from PIL import Image, ImageChops
+from . import sixel
+
+def main():
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument('a')
+	parser.add_argument('b')
+	parser.add_argument('--abs', action='store_true')
+	parser.add_argument('-n', '--negative', action='store_true')
+	parser.add_argument('-o')
+	parser.add_argument('-b', '--background', action='store_true')
+
+	args = parser.parse_args()
+
+	a = np.asarray(Image.open(args.a)).astype(np.int16)
+	b = np.asarray(Image.open(args.b)).astype(np.int16)
+
+	if a.shape != b.shape:
+		print('cannot compare images of different sizes')
+		return
+
+
+	diff = np.abs(a-b) if args.abs else np.maximum(0, b-a) if args.negative else np.maximum(0, a-b)
+	diff[:,:,3] = 255
+
+
+	ret = Image.fromarray(diff.astype(np.uint8))
+
+	if (not args.background) and sixel.init():
+		print(sixel.to_sixel(ret), end='')
+
+	if args.o:
+		ret.save(args.o)
+
+
+if __name__ == '__main__':
+	main()
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='imgdiff',
+	version='1.0',
+	url='https://github.com/trnciii/imgdiff',
+	license='MIT',
+	packages={'':['imgdiff/*']},
+	install_requires=[
+		'numpy',
+		'libsixel-python',
+		'Pillow',
+	],
+	entry_points={
+		'console_scripts': [
+			'imgdiff = imgdiff.__main__:main'
+		]
+	}
+)
